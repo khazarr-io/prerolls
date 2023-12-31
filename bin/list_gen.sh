a175caded75085a0e5efccfcdb27fa4d161466f0
Khazarr <noreply@khazarr.io>
2023-12-30 22:32:24 -0800

    [Change] Removal of ending semicolon [Change] Use shellcheck

--- bin/list_gen.sh
+++ bin/list_gen.sh
@@ -4,16 +4,24 @@ dest=/data/prerolls
 files=()
 sources=(fiverr ivipid priyansh)
 
-for source in ${sources[@]};
+for source in "${sources[@]}"
 do
-  for file in ../${source}/*
+  for file in ../"${source}"/*
   do
     f=$(echo "${file}" | cut -d/ -f 2-)
     files+=("${f}")
   done
 done
 
-for file in ${files[@]}
+count=0
+total=$(( ${#files[@]} - 1 ))
+
+for file in "${files[@]}"
 do
-  echo -n "${dest}/${file};"
+  echo -n "${dest}/${file}"
+  if [[ "${count}" -ne "${total}" ]]
+  then
+    echo -n ";"
+    count=$(( count + 1 ))
+  fi
 done
